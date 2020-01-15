b3c63197b97c7d12b0a692b37f1f1aab46e99528
Igor Ostapenko <ihoro@users.noreply.github.com>
2020-01-15 04:48:47 +0200

    Setup windows build

--- e2e/tar-stream-chunker.spec.js
+++ e2e/tar-stream-chunker.spec.js
@@ -3,7 +3,7 @@
 const t = require('./scaffold/t');
 const { tap } = require('rxjs/operators');
 
-const cmd = `../tar_stream_chunker`;
+const cmd = `${process.env.E2E_CMD_PREFIX} ../tar_stream_chunker`;
 
 test('should print usage help if no params provided', t.pipe(
   t.run .exec(cmd),
