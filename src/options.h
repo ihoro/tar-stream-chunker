979d8cc0d9c1899e133568404223069ded10c799
Igor Ostapenko <ihoro@users.noreply.github.com>
2020-09-03 11:01:05 +0300

    options.h: use "extern" for portability

--- src/options.h
+++ src/options.h
@@ -1,8 +1,8 @@
 #ifndef _OPTIONS_H_
 #define _OPTIONS_H_
 
-char	*file_name;
-int	chunk_size;
-void	parse_options(int argc, char *argv[]);
+extern char	*file_name;
+extern int	chunk_size;
+extern void	parse_options(int argc, char *argv[]);
 
 #endif  /* _OPTIONS_H_ */
